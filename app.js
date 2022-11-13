e617ff4f7c3dfcbb9c85190d9a2dbd2e4afac6fa
am1671 <adityam.1671@gmail.com>
2022-11-13 21:17:38 +0530

    site before trying animations

---
+++ app.js
@@ -0,0 +1,13 @@
+VANTA.NET({
+    el: "#main",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 240.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xed155f,
+    backgroundColor: 0x23153c,
+    points: 16.00
+  })
