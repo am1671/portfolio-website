1b9f3cf95418507dd72bc6f80bbada6a2f9874e2
am1671 <adityam.1671@gmail.com>
2022-11-14 16:04:13 +0530

    min width etc adjust

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 VANTA.NET({
-    el: "#main",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 20.00,
-    minWidth: 30.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0xed155f,
-    backgroundColor: 0x23153c,
-    points: 16.00
-  })
+  el: "#main",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xed155f,
+  backgroundColor: 0x23153c,
+  points: 16.0,
+});
